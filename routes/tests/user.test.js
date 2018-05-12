511b023d29e6ade3d14d5270ab729c8207eedd2a
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2018-05-12 10:45:53 -0700

    Created a get route to get user by id, in doing so also created a authentication middleware to protect the routes, you can only find user(yourself) if you are logged in, this will be key for making the firebase route which will only let you post if you are authenticated. Next is making the logout

--- routes/tests/user.test.js
+++ routes/tests/user.test.js
@@ -1,10 +1,16 @@
+const jwt = require('jsonwebtoken');
+const request = require('supertest');
+var chai = require('chai'),
+    assert = chai.assert,
+    expect = chai.expect,
+    should = chai.should;
+
+var chaiHttp = require('chai-http');
+
 const {
     ObjectID
 } = require('mongodb');
 
-const jwt = require('jsonwebtoken');
-const expect = require('expect');
-const request = require('supertest');
 const {
     users,
     populateUsers
@@ -20,18 +26,118 @@ const {
 } = require('../../models/user.js');
 
 
+chai.use(chaiHttp);
 
-populateUsers();
+// before(populateUsers);
 
-// describe('POST api/user/register'), () => {
-//     it('should register a new user', (done) => {
-//         var text = 'Test todo text';
-//         request(router)
-//             .post('api/user/register')
-//             .send({
-//                 text
-//             })
-//             .expect(200)
+describe('POST api/user/register', () => {
+    it('should register and create a new user', (done) => {
+        const user = {
+            firstName: "Joshi",
+            lastName: "Blankenstein",
+            email: "jb@gmail.com",
+            password: "fiddlesticks"
+        }
 
-//     })
-// }
+        chai.request(app)
+            .post('/api/user/register')
+            .send(user)
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(400);
+                res.body.should.be.a('object');
+                res.body.book.should.have.property('firstName');
+                res.body.book.should.have.property('lastName');
+                res.body.book.should.have.property('email');
+                res.body.book.should.have.property('password');
+                expect(res).to.have.header('x-auth');
+                User.findOne({
+                    email: 'jb@gmail.com'
+                }).then(user => {
+                    expect(user).toExist()
+                    expect(user.password).toNotBe(password)
+                }).catch((err) => done(err));
+                done();
+            });
+
+    });
+
+    it('should return errors if request is invalid', (done) => {
+        chai.request(app)
+            .post('/api/user/register')
+            .send({
+                email: 'monkey',
+                password: "ble"
+            })
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(400);
+                done();
+            });
+
+    });
+
+    it('should not create user if email there is already the same email', (done) => {
+
+        chai.request(app)
+            .post('/api/user/register')
+            .send({
+                email: users[0].email
+            })
+            .end((err, res) => {
+
+                expect(res.statusCode).to.equal(400);
+                done();
+            });
+
+    });
+});
+
+describe('POST api/user/login', () => {
+
+    it('should login user and return auth token', (done) => {
+        chai.request(app)
+            .post('/api/user/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+                expect(res.statusCode).to.equal(200);
+                expect(res).to.have.header('x-auth');
+                User.findById(users[1]._id)
+                    .then(user => {
+                        // expect(user.token[0].toInclude({
+                        //     access: 'auth',
+                        //     token: res.headers['x-auth']
+                        // }));
+                        done();
+                    }).catch((err) => done(err))
+            });
+
+    });
+
+    it('should reject invalid login', (done) => {
+        chai.request(app)
+            .post('/api/user/login')
+            .send({
+                email: users[1].email,
+                password: "notpassword"
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+                expect(res.statusCode).to.equal(400);
+                expect(res).to.not.have.header('x-auth');
+                User.findById(users[1]._id)
+                    .then(user => {
+                        // expect(user.token.length).toBe(0);
+                        done();
+                    }).catch((err) => done(err));
+
+            });
+    });
+
+});
