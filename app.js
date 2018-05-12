3d0a297414b0e99895019c120742a17ed6ed4304
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2018-05-11 17:58:43 -0700

    I got dummy data to go into the dummy database, I also refactored app.js and created a config file for it. Next I will create the test routes

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-let env = process.env.NODE_ENV || 'development';
-console.log(env);
-if (env === "development") {
-  process.env.PORT = 4000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/AddressBook';
-} else if (env === 'test') {
-  console.log("TEST")
-  process.env.PORT = 4000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/AddressBookTest';
-}
-
+require('./server.config.js');
 
 const express = require('express');
 const bp = require('body-parser');
--- routes/tests/user.test.js
+++ routes/tests/user.test.js
@@ -6,62 +6,22 @@ const jwt = require('jsonwebtoken');
 const expect = require('expect');
 const request = require('supertest');
 const {
+    users,
+    populateUsers
+} = require('./seed/seed');
+
+const {
     app
 } = require('../../app.js');
-const {
-    JWT_SECRET
-} = require('../../config/config.js');
 
-// const {
-//     router
-// } = require('../user.js');
 
 const {
     User
 } = require('../../models/user.js');
 
-const userOneId = new ObjectID();
-const userTwoId = new ObjectID();
-
-console.log("anything?")
-const users = [{
-    _id: userOneId,
-    firstName: "Jman",
-    lastName: "MgGilicuty",
-    email: 'vallejomon@gmail.com',
-    password: 'mypassword',
-    tokens: [{
-        access: 'auth',
-        token: jwt.sign({
-            _id: userOneId,
-            access: 'auth'
-        }, JWT_SECRET).toString()
-    }]
-}, {
-    _id: userTwoId,
-    firstName: "Day",
-    lastName: "Man",
-    email: 'dayman@gmail.com',
-    password: 'mypassword2',
-}];
 
 
-var populateUsers = (done) => {
-    console.log("DATATIME")
-    User.remove({}).then(() => {
-        var userOne = new User(users[0]).save();
-        var userTwo = new User(users[1]).save();
-        console.log(userOne, "user")
-        return Promise.all([userOne, userTwo])
-    }).then(() => done());
-};
-
-
-
-
-beforeEach(populateUsers);
-
-
+populateUsers();
 
 // describe('POST api/user/register'), () => {
 //     it('should register a new user', (done) => {
