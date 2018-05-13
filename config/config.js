028dc73275d4dba6f6140acc0443cae0507752b8
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2018-05-12 22:00:52 -0700

    travis test

---
+++ config/config.js
@@ -0,0 +1,16 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAxf_BJK-5WdWdiYnSqb7m3rkAGQaXRckg",
+  authDomain: "strv-addressbook-3eeb1.firebaseapp.com",
+  databaseURL: "https://strv-addressbook-3eeb1.firebaseio.com",
+  projectId: "strv-addressbook-3eeb1",
+  storageBucket: "strv-addressbook-3eeb1.appspot.com",
+  messagingSenderId: "828637021612"
+};
+
+const JWT_SECRET = 'I follow a cat profile on instagram';
+
+
+module.exports = {
+  FIREBASE_CONFIG,
+  JWT_SECRET
+};
