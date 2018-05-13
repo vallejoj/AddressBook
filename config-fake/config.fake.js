341331a1b71fe4a5b328f172fe676ea603094437
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2018-05-12 19:52:57 -0700

    Created a fake config file so that one could put in their own config info with ease.

---
+++ config-fake/config.fake.js
@@ -0,0 +1,15 @@
+const FIREBASE_CONFIG = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
+
+const JWT_SECRET = 'Enter your secret here';
+
+module.exports = {
+  FIREBASE_CONFIG,
+  JWT_SECRET
+};
